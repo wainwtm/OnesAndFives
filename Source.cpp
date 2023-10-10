18e60128ead9bb196e1d3d48aa05f005210e265a
wainwtm <timothy.wainwright@gmail.com>
2023-10-10 21:11:18 +0100

    First (!) addition

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "functions.h"
+#include <map>
+#include <thread>
+using namespace std;
+
+
+
+
+int main() {
+	
+	valueiterate(0.001,10000);
+	//cout<<oneplayergame();
+
+}
